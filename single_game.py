d6f81796d843dc5cd5e85a732a54c328a2d33eb4
felixM2020 <94495885+felixM2020@users.noreply.github.com>
2022-10-04 18:49:34 +1100

    Update single_game.py
    Added match, batting and bowling columns

--- single_game.py
+++ single_game.py
@@ -1,1 +1,11 @@
+# Match information
 
+match_columns = ['result_string', 'left_team', 'right_team', 'location', 'toss_won_by', 'batted_first', 'competition_string', 'date']
+
+# Batting stats
+
+bat_columns = ['unclean_name', 'bat_id', 'team', 'runs', 'balls', 'wicket_string']
+
+# Bowling stats
+
+bowl_columns = ['unclean_name', 'bowl_id', 'team', 'wicket_string', 'over_string']
